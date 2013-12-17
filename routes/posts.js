5a258301578aa5a261cf3a7a93da93dbe617444f
GIDEON ROSENTHAL <gideonrosenthal@GIDEONs-iMac.local>
2013-12-16 21:17:45 -0800

    changing posts 2.0

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
 var Server = require('mongodb').Server;
-var db;
+var myDb;
 var mongo = require('mongodb');
 var BSON = mongo.BSONPure;
 
-
-var mongoClient = new MongoClient(new Server('50.201.36.154', 27017));
-mongoClient.connect(function(err, mongoClient) {
-        db = mongoClient.db("reddit");
-        db.collection('nba', {strict:true}, function(err, collection){
+MongoClient.connect('mongodb://54.201.36.154/reddit', function(err, db) {
+		myDb = db;
         if(err) {
                 console.log("There was a problem with the database");
         }
@@ -21,7 +18,7 @@ mongoClient.connect(function(err, mongoClient) {
 exports.findAll = function(req, res) {
     console.log("Find all console test");
     var name = req.query["name"];
-    db.collection('nba', function(err, collection) { 
+    myDb.collection('nba', function(err, collection) { 
             collection.find({position:{$gt:-1}}, {position:1, title:1, url:1, author:1}).sort({position:1}).toArray(function(err, items) {
                 console.log(items)
 		res.jsonp(items);
@@ -33,7 +30,7 @@ exports.findOne = function(req, res){
 	var myPosition = req.params.position;
 	myPosition = parseInt(myPosition); 
 	var query = {position: myPosition};
-	db.collection('nba').findOne(query, function(err, document) {
+	myDb.collection('nba').findOne(query, function(err, document) {
 		if (err) throw err; 
 		var toReturn = ([document.position, document.title, document.url, document.author]);
 		res.send(toReturn);			 
