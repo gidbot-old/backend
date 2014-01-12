05d214c03100519a310431c1bd64239508b5304f
GIDEON ROSENTHAL <gideonrosenthal@GIDEONs-iMac.local>
2014-01-12 01:46:09 -0800

    fixing mongo

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,8 @@ var myDb;
 var mongo = require('mongodb');
 var BSON = mongo.BSONPure;
 
-MongoClient.connect('mongodb://54.201.36.154/reddit', function(err, db) {
+//mongodb://54.201.36.154/reddit
+MongoClient.connect('mongodb://54.201.114.29/reddit, function(err, db) {
 		myDb = db;
         if(err) {
                 console.log("There was a problem with the database");
